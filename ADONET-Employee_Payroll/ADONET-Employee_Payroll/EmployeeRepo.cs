3b2919bf4dd4bac54130a8f020abfbba56a28f3d
Reshma2310 <dreshma253@gmail.com>
2022-06-22 10:50:31 +0530

    [RESHMA] - Check if database connection is established or not

---
+++ ADONET-Employee_Payroll/ADONET-Employee_Payroll/EmployeeRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET_Employee_Payroll
+{
+    public class EmployeeRepo
+    {
+        public static string dbpath = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Payroll_Service;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(dbpath);        
+        public void ConnectionOfDatabase()
+        {
+            try
+            {
+                connect.Open();
+                using (connect)
+                {
+                    Console.WriteLine("Database connectivity successful.");
+                }
+                connect.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Database connectivity failed");
+            }
+        }
+    }
+}
---
+++ ADONET-Employee_Payroll/ADONET-Employee_Payroll/Program.cs
@@ -0,0 +1,2 @@
+﻿ADONET_Employee_Payroll.EmployeeRepo payroll = new ADONET_Employee_Payroll.EmployeeRepo();
+payroll.ConnectionOfDatabase();
