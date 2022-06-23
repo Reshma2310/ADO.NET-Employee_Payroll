b796186272d6b56944f80c133784ef0627b07e4a
Reshma2310 <dreshma253@gmail.com>
2022-06-23 22:48:13 +0530

    [RESHMA] - Creating & Inserting Employee Details into Database

--- ADONET-Employee_Payroll/ADONET-Employee_Payroll/EmployeeRepo.cs
+++ ADONET-Employee_Payroll/ADONET-Employee_Payroll/EmployeeRepo.cs
@@ -41,7 +41,7 @@ namespace ADONET_Employee_Payroll
                 SqlDataReader reader = command.ExecuteReader();
                 if (reader.HasRows)
                 {
-                    Console.WriteLine("ID\tName\t\tSalary\t\tDate\t\t\t\tGender\n");
+                    Console.WriteLine("ID\tName\t\t\tSalary\t\t\tDate\t\t\t\tGender\n");
                     while (reader.Read())
                     {
                         model.ID = reader.GetInt32(0);
@@ -49,7 +49,7 @@ namespace ADONET_Employee_Payroll
                         model.Salary = reader.GetDouble(2);
                         model.Start = reader.GetDateTime(3);
                         model.Gender = reader.GetString(4);
-                        Console.WriteLine(model.ID + "\t" + model.Name + "\t\t" + model.Salary + "\t\t" + model.Start + "\t\t" + model.Gender);
+                        Console.WriteLine(model.ID + "\t" + model.Name + "\t\t\t" + model.Salary + "\t\t\t" + model.Start + "\t\t\t" + model.Gender);
                     }
                 }
                 else
@@ -74,10 +74,10 @@ namespace ADONET_Employee_Payroll
                 Console.WriteLine("Enter Salary");
                 model.Salary = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Enter a Year,Month,Date");
-                model.Start = DateTime.Now;
+                model.Start = Convert.ToDateTime(Console.ReadLine());
                 Console.WriteLine("Enter Gender");
                 model.Gender = Console.ReadLine();
-                SqlCommand sql = new SqlCommand("SP_Employee_Payroll", connect);                    
+                SqlCommand sql = new SqlCommand("SPEmployeePayRoll", connect);                    
                 sql.CommandType = CommandType.StoredProcedure;
                 sql.Parameters.AddWithValue("@NAME", model.Name);
                 sql.Parameters.AddWithValue("@SALARY", model.Salary);
@@ -111,14 +111,30 @@ namespace ADONET_Employee_Payroll
                 Console.WriteLine("Details are not updated");
             }
         }
-        public void deleteEmployeeDetails()
+        public void DeleteEmployeeDetails()
         {
-            SqlConnection connect = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Payroll_Service;Integrated Security=True");
-            connect.Open();
-            string query = @"DELETE FROM Employee_Payroll where ID = '20'";
-            SqlCommand cmd = new SqlCommand(query, connect);
-            object res = cmd.ExecuteScalar();
-            connect.Close();
+            SqlConnection connect = new SqlConnection(dbpath);
+            try
+            {
+                using (connect)
+                {
+                    connect.Open();
+                    Console.WriteLine("Enter name of employee to delete from records:");
+                    string name = Console.ReadLine();
+                    string query = "delete from employee_payroll where name='" + name + "'";
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.ExecuteNonQuery();
+                    connect.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connect.Close();
+            }
 
         }
     }
--- ADONET-Employee_Payroll/ADONET-Employee_Payroll/Program.cs
+++ ADONET-Employee_Payroll/ADONET-Employee_Payroll/Program.cs
@@ -1,7 +1,27 @@
 ﻿ADONET_Employee_Payroll.EmployeeRepo payroll = new ADONET_Employee_Payroll.EmployeeRepo();
 ADONET_Employee_Payroll.PayrollModelClass modelClass = new ADONET_Employee_Payroll.PayrollModelClass();
 payroll.ConnectionOfDatabase();
-//payroll.CreateNewContact();
-//payroll.updateDetails();
-payroll.deleteEmployeeDetails();
-payroll.RetrieveDataFromDatabase();
+Console.WriteLine("Enter your option to perform\n1. Create New Employee Details\n2. Update the Details\n3. View the Employee Details\n4. Delete the Details using name");
+int input = Convert.ToInt32(Console.ReadLine());
+switch (input)
+{
+    case 1:
+        payroll.CreateNewContact();
+        payroll.RetrieveDataFromDatabase();
+        break;
+    case 2:
+        payroll.updateDetails();
+        payroll.RetrieveDataFromDatabase();
+        break;
+    case 3:
+        payroll.RetrieveDataFromDatabase();
+        break;
+    case 4:
+        payroll.RetrieveDataFromDatabase();
+        payroll.DeleteEmployeeDetails();
+        payroll.RetrieveDataFromDatabase();
+        break;
+    default:
+        Console.WriteLine("Invalid Input");
+        break;
+}
